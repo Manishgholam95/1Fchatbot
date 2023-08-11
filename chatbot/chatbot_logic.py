432f410603b79ccedbd1f1b12df39ba8742438f9
manish gholam <manish.gholam@atriina.com>
2023-08-12 01:14:24 +0530

    add context code

--- chatbot/chatbot_logic.py
+++ chatbot/chatbot_logic.py
@@ -5,15 +5,39 @@ import dotenv
 
 dotenv.load_dotenv()
 
+chat_history = []
+
+import re
+
+def is_query_related_to_history(query, history):
+    """
+    Determine if the query is related to the recent history.
+    """
+    # Extract keywords from the recent history
+    history_keywords = set(re.findall(r'\b\w+\b', history.lower()))
+    
+    # Check if any of the keywords appear in the query
+    for keyword in history_keywords:
+        if keyword in query.lower():
+            return True
+    return False
+
+def get_recent_history_as_context(num_messages=5):
+    """
+    Get the last few messages (both user and bot) as a single string.
+    """
+    recent_history = chat_history[-num_messages:]
+    return "\n".join([f"{sender if sender != 'bot' else ''}: {message}" for sender, message in recent_history])
+
 def construct_index():
     # set maximum input size
-    max_input_size = 4000
+    max_input_size = 500
     # set number of output tokens
-    num_outputs = 4000
+    num_outputs = 500
     # set maximum chunk overlap
     max_chunk_overlap = 20
     # set chunk size limit
-    chunk_size_limit = 4050
+    chunk_size_limit = 600
 
     openai_api_key = os.getenv('OPENAI_API_KEY')
 
@@ -34,9 +58,28 @@ def construct_index():
     return index
 
 def get_response(query):
+    # First, try to get an answer without any context
     if not os.path.exists('index.json'):
         construct_index()
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    response = index.query(query)
-    print(response.response)
+    response = index.query("User: " + query)
+
+    # If the response indicates uncertainty, use chat history as context
+    uncertain_responses = [
+        "I'm sorry, I'm not sure what your question or query is related to",
+        "I don't know"
+    ]
+    if any(phrase in response.response for phrase in uncertain_responses):
+        # Get the recent chat history as context
+        context = get_recent_history_as_context()
+        # If the query is related to history, combine the context with the current user query
+        if is_query_related_to_history(query, context):
+            full_query = context + "\nUser: " + query
+            response = index.query(full_query)
+
+    # Save the interaction to chat history
+    chat_history.append(("user", query))
+    chat_history.append(("bot", response.response))
+    
+
     return response.response
